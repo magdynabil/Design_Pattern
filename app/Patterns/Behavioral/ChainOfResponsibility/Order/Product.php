b91de792703c92dcd2def4e1f7db9a6ad1af7945
magdynabil <magdynabil.officail@gmail.com>
2024-08-28 17:14:36 +0300

    CHAIN OF RESPONSIBILITY DESIGN PATTERN

---
+++ app/Patterns/Behavioral/ChainOfResponsibility/Order/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Patterns\Behavioral\ChainOfResponsibility\Order;
+
+class Product
+{
+    private string $id;
+    private string $name;
+
+    public function __construct(string $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
+
